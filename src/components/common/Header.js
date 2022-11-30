d8cdacd11870d4b5dc6cdd93a6a7dcb16ff9099a
aguirre739 <pame11_@live.com>
2022-11-30 01:03:15 -0300

    agrego json server, json y agregar producto2

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,19 +2,20 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import {NavLink} from 'react-router-dom';
 
 const Header = () =>{
   return (
     <div>
         <Navbar collapseOnSelect expand="lg" variant="dark" className="mb-3 navbar1">
         <Container>
-            <Navbar.Brand className="neon flicker-slow my-2" href="#home">Games Box</Navbar.Brand>
+            <Navbar.Brand className="neon flicker-slow my-2" href="#">Games Box</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="#tienda">Tienda</Nav.Link>
-                    <Nav.Link href="#nuevoJuego">Nuevo Juego</Nav.Link>
-                    <Nav.Link href="#login">Iniciar Sesión</Nav.Link>
+                    <NavLink exact={true} to="/" className="nav-link">Inicio</NavLink>
+                    <NavLink exact={true} to="/productos" className="nav-link">Tienda</NavLink>
+                    <NavLink exact={true} to="/productos/nuevo" className="nav-link">Nuevo Juego</NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Container>
--- src/components/productos/AgregarProducto.js
+++ src/components/productos/AgregarProducto.js
@@ -1,10 +1,89 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const AgregarProducto = () => {
     return (
-        <div>
-            <h2>Pagina agregar producto</h2>
-        </div>
+        <Container className='color-texto d-flex flex-column justify-content-center align-items-center'>
+            <h1 className='text-center my-4'>Agregar Nuevo Juego</h1>
+                <Form className='w-75 fondo-form shadow-lg p-3 mb-5 rounded'>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Nombre del juego *</Form.Label>
+                        <Form.Control type="text" placeholder="Ej. Isacc" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3">
+                        <Form.Label>Precio *</Form.Label>
+                        <Form.Control type="number" placeholder="Ej. $2600" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3">
+                        <Form.Label>Portada del juego *</Form.Label>
+                        <Form.Control type="text" placeholder="url/link de la imagen" />
+                    </Form.Group>
+                    
+                    <Form.Group className="mb-3">
+                        <Form.Label>Fecha de lanzamiento *</Form.Label>
+                        <Form.Control type="date" placeholder="fecha" />
+                    </Form.Group>
+
+                    <h3 className="text-center my-4">Categorias</h3>
+                    <Form.Group className="my-3 text-center">
+                        <div className="mb-3">
+                            <Form.Check
+                            type="radio"
+                            inline
+                            label="Acción"
+                            value="accion"
+                            name="categoria"
+                            />
+                            <Form.Check
+                            type="radio"
+                            inline
+                            label="Aventura"
+                            value="aventura"
+                            name="categoria"
+                            />
+                            <Form.Check
+                            type="radio"
+                            inline
+                            label="Rol"
+                            value="rol"
+                            name="categoria"
+                            />
+                            <Form.Check
+                            type="radio"
+                            inline
+                            label="Simulación"
+                            value="simulacion"
+                            name="categoria"
+                            />
+                            <Form.Check
+                            type="radio"
+                            inline
+                            label="Estrategia"
+                            value="estrategia"
+                            name="categoria"
+                            />
+                            <Form.Check
+                            type="radio"
+                            inline
+                            label="Deportes"
+                            value="deporte"
+                            name="categoria"
+                            />
+                        </div>
+                    </Form.Group>
+
+                    <div className='d-grid gap-2'>
+                        <Button className='boton' size="sm" block type="submit">
+                            Agregar Juego
+                        </Button>
+                    </div>
+                    
+                </Form>
+        </Container>
     );
 };
 
